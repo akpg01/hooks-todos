ed9103e44166ff8e16c5a0b37bf60eb629a4e980
Grace Akpan <opps4lrng@gmail.com>
2019-11-29 12:04:16 -0700

    implemented a todo list app with the use of Reacthooks

---
+++ src/components/TodoForm.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useContext } from 'react';
+import TodosContext from '../context';
+import axios from 'axios';
+import uuidv4 from 'uuid/v4';
+
+export default function TodoForm() {
+    const [todo, setTodo] = useState("");
+    const { state: { todos, currentTodo = {} }, dispatch } = useContext(TodosContext);
+
+    useEffect(() => {
+        if (currentTodo.text) {
+            setTodo(currentTodo.text)
+        } else {
+            setTodo('');
+        }
+    }, [currentTodo.id]);
+
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        if (currentTodo.text) {
+            const response = await axios.patch(`https://hooks-api-master.opps4lrng.now.sh/todos/${currentTodo.id}`, {
+                text: todo,
+            });
+            dispatch({ type: "UPDATE_TODO", payload: response.data })
+        } else {
+            if (todo.length === 0 ||
+                (todos.findIndex(t => t.text.toLowerCase() === todo.toLowerCase()) > -1)) {
+                return;
+            }
+
+            const response = await axios.post(`https://hooks-api-master.opps4lrng.now.sh/todos`, {
+                id: uuidv4(),
+                text: todo,
+                complete: false
+            });
+            dispatch({ type: "ADD_TODO", payload: response.data });
+        }
+
+        setTodo("");
+    }
+
+    return (
+        <form className="flex justify-center p-5" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="border-black border-solid border-2"
+                onChange={event => setTodo(event.target.value)}
+                value={todo}
+            />
+        </form>
+    )
+}
